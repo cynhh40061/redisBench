2573b250c225c4d2bd53f8100405d9f9ae2cadfd
Niko <niko@clctech.co>
2018-11-21 15:42:04 +0800

    add tag for myself

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const getAndDeleteKeys = async (RedisManger) => {
     for (let index = 0; index < 10; index++) {
         try {
             // 找出哪個keys的Slot桶已經超過10000筆以上未處理, 優先處理
-            const key = await RedisManger.lpop(CHANNEL);
+            const key = await RedisManger.lpop(CHANNEL);//從CHANNEL的value左邊開始取值
             await RedisManger.get(key);
             await RedisManger.del(key);
         } catch (error) {
@@ -46,29 +46,30 @@ Aigle.attempt(() => {//非同步 Pormise 處理的套件
 })
     .then(async () => {
         const NS_PER_SEC = 1e9;//10億
-        const MS_PER_NS = 1e-6;
-        const countingTime = process.hrtime();
+        const MS_PER_NS = 1e-6;//100萬分之1
+        const countingTime = process.hrtime();//取得精確時間(也可用uptime()的精確度低一點，較快)
         let diff;
         let loop;
         const initTimes = await RedisContext.getRedis().get(TOTALTIMES);
-        const continuSec = 1;
+        const continuSec = 10;
         const onceSec = 1;
 
         try {
+            await RedisContext.getRedis().del('RANDOMLIST');//show完刪除list
             loop = setInterval(async function () {//settimeout
-                await setKeys(RedisContext.getRedis(), 10).catch((err) => { throw err; });//塞
+                await setKeys(RedisContext.getRedis(), 1).catch((err) => { throw err; });//塞
                 await getAndDeleteKeys(RedisContext.getRedis()).catch((err) => { throw err; });//塞完刪除
-                diff = process.hrtime(countingTime);
+                diff = process.hrtime(countingTime);//精確時間差
                 if (((diff[0] * NS_PER_SEC + diff[1]) * MS_PER_NS) > (continuSec * 1000)) {
                     clearInterval(loop);
-                    console.log('LoopTimes: ', (await RedisContext.getRedis().get(TOTALTIMES) - initTimes));
-                    console.log('TotalCastTimes: ', ((diff[0] * NS_PER_SEC + diff[1]) * MS_PER_NS));
-                    await RedisContext.getRedis().lrange('RANDOMLIST', 0, -1).then(
+                    console.log('LoopTimes: ', (await RedisContext.getRedis().get(TOTALTIMES) - initTimes));//show total count，這邊count沒做初始化，直接用終值減初值去計次
+                    console.log('TotalCastTimes: ', ((diff[0] * NS_PER_SEC + diff[1]) * MS_PER_NS));//diff[0]：秒 + diff[1]：微秒
+                    await RedisContext.getRedis().lrange('RANDOMLIST', 0, -1).then(//獲取RANDOMLIST從index[0]~最後一筆所有值
                         (req) => console.log('randomList: ', req),
                         (e) => console.error(e)
                     );
-                    await RedisContext.getRedis().del('RANDOMLIST');
                 }
+                console.log(diff, '=================', ((diff[0] * NS_PER_SEC + diff[1]) * MS_PER_NS));
             }, (onceSec * 1000));
         } catch (error) {
             console.log(error);
